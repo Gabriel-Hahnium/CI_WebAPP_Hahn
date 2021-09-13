7f832f676baf64ec18a239efd99900831458c0ae
Gabriel-Hahnium <gabriel.hahn.a@gmail.com>
2021-09-13 16:04:29 -0500

    unittest cleanup

--- app_test.py
+++ app_test.py
@@ -7,7 +7,6 @@ class ButtonPress_test(unittest.TestCase):
     tester = app.test_client(self)
     response = tester.get('/random_verb')
     self.assertEqual(response.status_code, 200)
-    print(response)
 
 if __name__ == '__main__':
     unittest.main()
